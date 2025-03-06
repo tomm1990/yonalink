7aaf908833bbea2e141b5480160a77920a8945db
Tom Goldberg <tomg3210@gmail.com>
2025-03-06 13:19:44 +0200

    yl-back (refactor) `setupAssociations()` to include both associations

--- yl-back/src/models/associations.ts
+++ yl-back/src/models/associations.ts
@@ -1,9 +1,7 @@
 import Expense from './Expense';
 import Category from './Category';
 
-Category.hasMany(Expense, { foreignKey: 'categoryId', as: 'expenses' });
-Expense.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
-
 export default function setupAssociations() {
-    console.log('Associations have been set up.');
+    Category.hasMany(Expense, { foreignKey: 'categoryId', as: 'expenses' });
+    Expense.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
 }
