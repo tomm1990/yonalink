9a18a08cea2fa154152f348206aecdd85c680a1e
Tom Goldberg <tomg3210@gmail.com>
2025-03-09 13:34:05 +0200

    yl-front (chore) comments removal from `vite.config.ts`

--- yl-front/vite.config.ts
+++ yl-front/vite.config.ts
@@ -1,17 +1,5 @@
-import { pigment } from '@pigment-css/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { createTheme } from '@mui/material';
-
-/**
- * @type {import('@pigment-css/vite-plugin').PigmentOptions}
- */
-// const pigmentConfig = {
-//   transformLibraries: ['@mui/material'],
-//   theme: createTheme({
-//     cssVariables: true,
-//   }),
-// };
 
 // https://vite.dev/config/
 export default defineConfig({
