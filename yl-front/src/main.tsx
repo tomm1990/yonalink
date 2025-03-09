ab2384fa8af275021e04f12c300f7b65b70d6df6
Tom Goldberg <tomg3210@gmail.com>
2025-03-09 13:33:29 +0200

    yl-front (feat) npm `react-scan`

--- yl-front/src/main.tsx
+++ yl-front/src/main.tsx
@@ -1,6 +1,13 @@
+import { scan } from "react-scan"; // must be imported before React and React DOM
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { StrictMode } from 'react';
+
+// TODO install an extension instead
+scan({
+  enabled: false,
+  trackUnnecessaryRenders: true
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
